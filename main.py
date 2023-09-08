5dc1e0586e0ef685781f60beacf2d2ad400b41f7
lastaur0ch <joelxjohnson@gmail.com>
2023-09-08 10:40:07 +0100

    removed slash in query parameter

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ async def root():
   return { "message": "Use slack_name and track as query parameters e.g /api/?slack_name=jjxavier&track=backend" }
 
 
-@app.get("/api/")
+@app.get("/api")
 async def read_item(slack_name: str, track: str):
     x = datetime.datetime.utcnow()
     return {"slack_name": slack_name, "current_day": x.strftime("%A"), "utc_time": x.replace(microsecond=0).isoformat() + "Z", "track": track, "github_file_url": "https://github.com/lastaur0ch/1-name-track/blob/main/main.py", "github_repo_url": "https://github.com/lastaur0ch/1-name-track", "status_code": 200}
